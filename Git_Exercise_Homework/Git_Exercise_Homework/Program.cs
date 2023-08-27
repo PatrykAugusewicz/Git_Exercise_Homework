97f9b2ccdb65afc2d13a109302c514709dc9f496
Patryk Augusewicz <84743481+PatrykAugusewicz@users.noreply.github.com>
2023-08-27 16:23:12 +0100

    FisrstCommit

---
+++ Git_Exercise_Homework/Git_Exercise_Homework/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Git_Exercise_Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
